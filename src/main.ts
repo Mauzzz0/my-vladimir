352631017f45799c937698bbf9275c6c4af038cd
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-12-12 20:53:22 +0300

    Добавлен ts-node

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+
+
+console.log(123)
+console.log(123)
+console.log(123)
+console.log(123)
+console.log(123)
+console.log(123)
+console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
